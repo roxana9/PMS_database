2de32ee345516ed1659464657542ba7d512845dc
Roxana Zahedi Nasab <z5451123@ad.unsw.edu.au>
2023-09-11 18:32:53 +1000

    Initial commit

---
+++ Patient.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Patient]
+(
+	[Patient ID] INT NOT NULL PRIMARY KEY, 
+    [FirstName] text NOT NULL, 
+    [DOB] DATETIME NOT NULL, 
+    [MedicareID] TEXT NOT NULL, 
+    [City] TEXT NULL, 
+    [Age] NUMERIC NULL, 
+    [Info] BIT NULL
+)
